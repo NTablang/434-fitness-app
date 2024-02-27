df36019c1681489e92e914c56f8239cb65d95e96
Neil Kundagrami <90292424+neilkund@users.noreply.github.com>
2024-02-26 22:31:01 -0500

    Added changes to tab 5 & 6

--- src/tabs/colors.js
+++ src/tabs/colors.js
@@ -1,8 +1,23 @@
+import { Center } from '@chakra-ui/react'
 import React from 'react'
 
 function ColorsTab() {
   return (
-    <div>ColorsTab</div>
+    <><h1  style={{ fontSize: '30px', fontWeight: 'bold' }}>Color</h1>
+    
+    <div style={{ display: "flex", flexDirection: 'column', justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div style={{ backgroundColor: "black", color: "yellow", padding: "10px", width: '100%', justifyContent: "center", alignItems: "center" }}>
+        Hello There!
+      </div>
+
+      <div style={{ backgroundColor: "yellow", color: "purple", padding: "10px", width: '100%', justifyContent: "center", alignItems: "center" }}>
+        Hi Again!
+      </div>
+
+
+    </div></>
+    
+
   )
 }
 
